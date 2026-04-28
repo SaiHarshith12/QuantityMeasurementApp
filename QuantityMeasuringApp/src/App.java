14c1fc65ed5489e7f09dce8e7ce6ff4d2a18efc5
SaiHarshith12 <varanasisaiharshith@gmail.com>
2026-04-28 10:28:18 +0530

    SaiHarshith Added UC2

--- QuantityMeasuringApp/src/App.java
+++ QuantityMeasuringApp/src/App.java
@@ -1,25 +1,53 @@
 public class App {
 
-    // Inner class to represent Feet
+    // Feet class
     static class Feet {
         private final double value;
 
-        // Constructor
         public Feet(double value) {
             this.value = value;
         }
 
-        // Override equals method
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
-
             if (obj == null || getClass() != obj.getClass()) return false;
 
             Feet other = (Feet) obj;
-
             return Double.compare(this.value, other.value) == 0;
         }
+    }
+
+    // Inches class
+    static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    // Static method for Feet comparison
+    public static boolean compareFeet(double v1, double v2) {
+        Feet f1 = new Feet(v1);
+        Feet f2 = new Feet(v2);
+        return f1.equals(f2);
+    }
+
+    // Static method for Inches comparison
+    public static boolean compareInches(double v1, double v2) {
+        Inches i1 = new Inches(v1);
+        Inches i2 = new Inches(v2);
+        return i1.equals(i2);
     }
 
     // Main method
@@ -27,11 +55,12 @@ public class App {
 
         System.out.println("Welcome to Quantity Measurement App");
 
-        // Create objects
-        Feet f1 = new Feet(5.0);
-        Feet f2 = new Feet(5.0);
+        // Feet comparison
+        System.out.println("Feet equal: " + compareFeet(5.0, 5.0));
+        System.out.println("Feet equal: " + compareFeet(5.0, 6.0));
 
-        // Compare and print result
-        System.out.println("Are the two values equal? " + f1.equals(f2));
+        // Inches comparison
+        System.out.println("Inches equal: " + compareInches(12.0, 12.0));
+        System.out.println("Inches equal: " + compareInches(12.0, 10.0));
     }
 }
